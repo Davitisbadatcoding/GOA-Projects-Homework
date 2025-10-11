9450c1126be797c3bccf1f0c4d73ee8c9d1b6713
Davitisbadatcoding <davitiscool76@gmail.com>
2025-10-11 10:46:42 +0400

    Added New homework files

---
+++ Level 2/Classwork2.py
@@ -0,0 +1,10 @@
+print("Hello")
+print(1)
+print(1.9999999999)
+
+name = "Davit Patarkatsishvili"
+Age = "14 Years Old"
+favoritefood = "Pizza"
+favoritecolor = "Red"
+
+print(name , Age , favoritefood , favoritecolor)
---
+++ Level 2/Homework2.py
@@ -0,0 +1,30 @@
+print("Hello world")
+print("Yellow")
+print("Cheese")
+print("Austin")
+print("Baku")
+
+print("1")
+print("196")
+print("256")
+print("3025")
+print("-727")
+
+print("3.14159")
+print("2.25")
+print("1.99999")
+print("7.97843")
+print("-89.532")
+
+name = "Davit"
+age = 14
+location = "Tbilisi"
+battery = 90
+battery = 91
+
+print(name)
+print(age)
+print(location)
+print(battery)
+
+print(name, age, location, battery)
