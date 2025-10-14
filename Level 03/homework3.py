38cd7ffa0ea05c0868652267d14ade8555d73006
Davitisbadatcoding <davitiscool76@gmail.com>
2025-10-14 15:26:25 +0400

    new stuff

---
+++ Level 03/homework3.py
@@ -0,0 +1,48 @@
+num1 = 10
+num2 = 20
+num3 = 30 
+
+print(num1)
+print(num2)
+print(num3)
+
+length = 20 #Cm
+width = 10 #Cm
+
+#P=2*(a+b)
+print(length + width)
+#Half=30cm..
+half = 30 #Cm
+print(half * 2)
+#Or
+print((length + width) * 2)
+
+
+#Addition
+
+print(2+2)
+print(3+3)
+print(4+4.1)
+
+#Subtraction
+
+print(27-8)
+print(20-9)
+print(300-98.1)
+
+#Multiplication
+
+print(3*98)
+print(23*77)
+print(2.5*44)
+
+#Division
+
+print(6/3)
+print(22/2.5)
+print(345777/9.776)
+
+food = "Cheese"
+Appliance = "Cheese Grater"
+
+print("I'm using a " + Appliance + " to grate " + food + "!")
