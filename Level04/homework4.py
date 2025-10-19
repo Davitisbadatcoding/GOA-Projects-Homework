e0073281425ab92878ec943b15d0dc1ad6aea7eb
Davitisbadatcoding <davitiscool76@gmail.com>
2025-10-19 21:06:15 +0400

    Hw and Cw

---
+++ Level04/homework4.py
@@ -0,0 +1,28 @@
+ #komentari aris texti romelic kompiuteri ver kitxulobs, chven shegvizlia es vixmarot rom davakomentarot raghac codze
+ #komentari unda gaaketo # diezis nishnit
+ #Case-Sensitive aris swored codis wera, anu magalitad tu chven gvaqvs variable: name da shen tu daprinte print(Name), es ara sworea radgan didi N aqvs pataris nacvlad
+ #snake_case aris magalitad variablshi rom ginda gamoyo adgili: name davit araswore iqneba name_davit iqneba swore
+ 
+
+name_1 = "Davit"
+name_2 = "Alex"
+
+print(name_1 , name_2)
+ 
+ 
+ #bug aris raghac secdoma an raghac to ara swored mushaobs shen codshi
+ #debugging aris bugis povna codshi
+
+#araswore code
+
+
+#print("Hello World)
+#city -> "London"
+#pritn(City)
+
+#swore code
+
+
+print("Hello World")
+city = "London"
+print(city)
---
+++ Level06/homework6.py
@@ -0,0 +1,23 @@
+age = input("Your age Please!")
+print(age)
+#inputi aris informaci romelic shedis kompitershi rom daamushavos
+#outputi aris informaci romelic gadmocems kompiuteri
+number1 = int(input("Number 1:"))
+number2 = int(input("Number 2:"))
+print(number1 + number2)
+print(number1 - number2)
+print(number1 / number2)
+print(number1 * number2)
+
+favcolor = input("Favorite Color")
+favanimal = input("Favorite animal")
+favcountry = input("Favorite Country")
+
+print("My favorite color is" + " " + favcolor + ", " + "my favorite animal is a" + favanimal + ", " + "and my favorite country is the amazing country" + favcountry)
+
+MostFavCar = input("Most Favorite Car!")
+FavCar = input("Favorite Car!")
+IKindaOfLikeThisCar = input("Meehhh, i like this car maybe.....")
+IHateThisCar = input("I hate this Car!")
+
+print(MostFavCar)
