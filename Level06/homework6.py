3d65e0b659d15cc3daa460152fa2990ca102ca36
Davitisbadatcoding <davitiscool76@gmail.com>
2025-10-25 11:02:48 +0400

    hw6

---
+++ Level06/homework6.py
@@ -0,0 +1,20 @@
+number1 = int(input("Number 1: "))
+number2 = int(input("Number 2: ")) 
+number3 = int(input("Number 3: "))
+
+print( (number1 + number2 + number3) / 3 )
+
+Addition1 = int(input("Number for addition!: "))
+Addition2 = int(input("Number for addition!: "))
+
+print( Addition1 + Addition2 )
+
+#Data types aris magalitad string, integer, float, boolean and etc. 
+#String= "Hello"
+#Float= 2.0
+#integer= 2
+
+Name1 = input("Mothers Name: ")
+Name2 = input("Fathers Name: ")
+
+print( "I love my Mother whos called" + " " + Name1 + ", " + "i also love my father whos called" + " " + Name2 )
