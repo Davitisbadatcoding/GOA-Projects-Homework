50ce5936b26a72db2ca46146210f429a86b8c95d
Davitisbadatcoding <davitiscool76@gmail.com>
2025-11-01 05:43:06 +0000

    hw8

---
+++ Level08/classwork8.py
@@ -0,0 +1,28 @@
+name = input("Whats your name? ")
+print(name)
+
+number1 = int(input("Number 1 please "))
+number2 = int(input("Number 2 please "))
+
+print(number1 + number2)
+
+age = int(input("Age please "))
+print(age)
+print(type(age))
+
+One = input("Input a String! ")
+Two = input("Input another String! ")
+Three = int(input("Integer Please! "))
+Four = float(input("Input a float now! "))
+
+print(One)
+print(type(One))
+
+print(Two)
+print(type(Two))
+
+print(Three)
+print(type(Three))
+
+print(Four)
+print(type(Four))
---
+++ Level08/homework8.py
@@ -0,0 +1,75 @@
+#inputi aris informacia romelic kompiutershi shegvaqvs
+#outputi aris informacia romelic kompiuteri gadmogvcems
+#magalitad printershi shagvqvs informaci ra unda davbechdot (input), da gamodis dabechdili purceli (output)
+
+test = input("Input something ")
+print(type(test))
+print(test)
+
+########################################################################################################
+
+#Strings
+
+One = "Hello"
+Two = "World"
+Three = "Three"
+Four = "Hyprland"
+Five = "sudo pacman -S hyprland xdg-desktop-portal-hyprland waybar dunst rofi network-manager-applet"
+
+#Integers
+
+One1 = 1
+Two1 = 2
+Three1 = 3
+Four1 = 4
+Five1 = 5
+
+#Floats
+
+One2 = 1.1
+Two2 = 2.2
+Three2 = 3.14159265359
+Four2 = 4.4
+Five2 = 5.5
+
+########################################################################################################
+
+mrvariable1 = "Variable1"
+msvariable2 =  79
+mrsvariable3 = 478.23567
+
+print(mrvariable1)
+print(type(mrvariable1))
+
+print(msvariable2)
+print(type(msvariable2))
+
+print(mrsvariable3)
+print(type(mrsvariable3))
+
+########################################################################################################
+
+word1 = input("Favorite pizza topping ")
+word2 = int(input("Your age "))
+
+print( "My favorite pizza topping is " + word1 + ", " + "and i am " + str(word2) + " " + "years old" )
+
+########################################################################################################
+
+number1 = int(input("Number 1 "))
+number2 = int(input("Number 2 "))
+number3 = int(input("Number 3 "))
+number4 = int(input("Number 4 "))
+number5 = int(input("Number 5 "))
+
+print( ( number1 + number2 + number3 + number4 + number5 ) / 5 )
+
+########################################################################################################
+
+name = input("Name please! ")
+last_name = input("Last name please! ")
+age = int(input("Age "))
+height = int(input("Height (preferably in Cm) "))
+weight = int(input("Weight (preferably in kg) "))
+
+print( "My name is" + " " + name + " " + "and my last name is" + " " + last_name + "." + " " + "I am currently" + " " + str(age) + " " + "years old" + ", " + "my height is" + " " + str(height) + " " + "centimeters" + " " + "and i weigh" + " " + str(weight) + " " + "kilos" + ".")
