1a3ddf542c8e784b1ec7bf1a2c3d97f6db8e5f6c
Davitisbadatcoding <davitiscool76@gmail.com>
2025-11-08 07:49:31 +0000

    +hw9-10

---
+++ Level10/classwork10.py
@@ -0,0 +1,22 @@
+#binary aris 1 da 0-sgan shemdgari  (0=false da 1=True 2 shemdeg=True da stringebi= true)
+
+#>
+print(5>10)
+print(11>10)
+
+#<
+print(5<10)
+print(142354<1)
+
+#<=
+print(5<=5)
+print(5<=10)
+
+#>=
+print(5>=10)
+print(25>=5*5)
+
+#==
+print(5==5)
+print("Hello" == "Hello")
+
---
+++ Level10/homework10.py
@@ -0,0 +1,90 @@
+#boolean aris an True an False (anu martali da mcdari), chven es shegvizlia gamoviyenot rom shevamowmot to swore aris an matematikis gamosaxuleba da aseshemdeg
+#binary aris 1 da 0-sgan shemdgari  (0=false da 1=True 2 shemdeg=True da stringebi= true)
+
+print(bool(0)) #False
+print(bool(1))#True
+
+a = 3
+b = 2
+print(a == b)
+
+print("Using" + " " + str(">"))
+number1 = int(input("Input a number to compare "))
+number2 = int(input("Input another number to compare "))
+print( number1 > number2 )
+
+print("Using" + " " + str("<"))
+number3 = int(input("Input a number again "))
+number4 = int(input("Input a number again.... "))
+print( number3 < number4 )
+
+print("Comparing the word you will input to Python")
+word1 = input("Input a Word (String if we are talking like nerds) ")
+word2 = "Python"
+print( word1 == word2 )
+
+print("Comparing to 100")
+word3 = int(input("Another Number... "))
+print( word3 < 100 )
+
+print("Enter your Password")
+password = input("Input your Password here ")
+storedpassword = "Python123"
+print(password == storedpassword)
+
+print("Using" + " " + str(">,<,=="))
+num1 = int(input("Input a number "))
+num2 = int(input("Input another... number "))
+
+print(num1 > num2)
+print(num1 < num2)
+print(num1 == num2)
+
+one = input("Input a Desert ")
+two = input("Input an Ocean ")
+three = input("Input a Car.... ")
+four = input("Input a A random word ")
+five = input("Input your name ")
+print("There's a Desert called" + " " + one + ", " + "and for some reason the user inputed the.." + " " + two + ", " + "His/Her favorite car is the" + " " + three + ". " + "And his/her random word is.. (Drum roll)" + " " + four + ", " + "we should probably stop using his/her so his/her name is... (More drum roll?)" + " " + five + "."  )
+
+Anumber1 = int(input("Input a number (Why do we have to use so many number.... im running out of names for my variables) "))
+Anumber2 = int(input("Input another number "))
+Anumber3 = int(input("Input a number..... "))
+Anumber4 = int(input("Input another number... "))
+
+print((Anumber1 + Anumber2 + Anumber3 + Anumber4) / 4 )
+
+z = "Hello!"
+y = 1
+x = 1.21
+w = bool(0)
+
+print(z)
+print(type(z))
+
+print(y)
+print(type(y))
+
+print(x)
+print(type(x))
+
+print(w)
+print(type(w))
+
+Mrvariable1 = "Word"
+Mrvariable2 = "word"
+
+print ( Mrvariable1 == Mrvariable2 )
+
+MsVariable = "40"
+MsVariable1 = "90"
+MsVariable2 = "2346375634875647568"
+MsVariable3 = "1.333333333"
+
+print((int(MsVariable) + int(MsVariable1) + int(MsVariable2) + float(MsVariable3)) / 4 )
+
+Mr1 = 47
+Mr2 = 121
+Mr3 = 256
+
+print(str(Mr1) + str(Mr2) + str(Mr3))
