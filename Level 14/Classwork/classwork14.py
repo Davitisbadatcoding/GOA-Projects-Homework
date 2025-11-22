953968f82f98162fb68c32ef4c745e0422de7701
Davitisbadatcoding <davitiscool76@gmail.com>
2025-11-22 17:57:43 +0000

    A LOT of stuff...

---
+++ Level 14/Classwork/classwork14.py
@@ -0,0 +1,39 @@
+#flowchart aris grapikulad nachvenebi codis nabijebi
+
+#Declares Hello as a string in the variable a and prints it
+a = "Hello"
+print(a)
+
+#sequencing aris roca codes miyveba tanmidevroolad
+b = World
+print(type(b))
+print(b)
+
+#iteration aris roca codhi ar sruldeba sanam swore pasuxi and pirobebi miecema
+
+b = input("Input your name ")
+a = "Davit"
+print(a==b)
+#If it comes false
+print("try again")
+#....
+
+#selection aris roca programa aketebs raghac pirobas archevani 
+a = 8
+b = 9
+print(8>9)
+print(8<9)
+
+#psuedocode aris algoritmi adamianistvis ufro gasageb enaze
+#natural language aris adamianistvis gasageb enaze codis gareshe axsna
+
+print("Get butter and add it to your pan")
+print("Add sugar when its has heated ")
+print("Add flour and transfer to a bowl")
+print("Add chocolate chips")
+print("add salt")
+print("Mix")
+print("Bake")
+print("and... there")
+print("You have cookies!")
+
