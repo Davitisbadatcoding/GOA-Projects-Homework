174f3477660ae69477d462b7d2dca0a45b3ec27e
Davitisbadatcoding <davitiscool76@gmail.com>
2025-11-29 06:29:23 +0000

    Homework Mr 15!

---
+++ Level 14/Homework/homework14.py
@@ -0,0 +1,4 @@
+temp = int(input("Input your Temperature in Fahrenheit "))
+print("Beep Boop converting to Celsius!")
+#F = (C*9/5)+32
+print((temp-32)/(9/5))
---
+++ Level 16/Classwork/classwork16.py
@@ -0,0 +1,23 @@
+#for loopi aris aris cikli romelic ar mtaverdeba sanam miva dasadrul mnishvnelobamde
+#indentation aris sajhiro for loopistvis magis gareshe ar imushavebs, indentation aris gamonayofi codshi
+
+for one in range(0, 67):
+    print(one)
+
+for two in range(12, 87):
+    print(two)
+
+for three in range(4, 98, 2):
+    print(three)
+
+name = "Davit"
+for four in range(10):
+    print(name)
+
+word1 = input("Input a String ")
+for five in range(10):
+    print(word1)
+
+number1 = int(input("Input a Number! "))
+for six in range(number1):
+    print(six)
