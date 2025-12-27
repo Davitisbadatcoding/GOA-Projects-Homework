ec7fe177c7583463be1fef6604e141efd1567230
Davitisbadatcoding <davitiscool76@gmail.com>
2025-12-27 06:51:39 +0000

    <p>Homework 21-23</p>

---
+++ Level 22/Homework/homework22.py
@@ -0,0 +1,26 @@
+arr = ["Mercedes", "Bmw", "Volkswagen", "Porsche", "Lancia"]
+print(arr [0])
+print(arr [2])
+
+arr = [0, 2, 4, 6, 8, 10]
+print(arr [1])
+print(arr [3])
+
+arr = ["hello", "world", "cool", "hot", "warm" ]
+print(arr [0])
+print(arr [1])
+print(arr [2])
+print(arr [3])
+
+arr[2] = "Davit"
+print(arr[2])
+
+numbers = [40, 80, 120]
+print(numbers[0] + numbers[2])
+
+name = ["Davit", "Andria", "Giorgi", "Alex", "Misho", "Sandro", "Mike"]
+
+name[0] = "John"
+name[4] = "Ben"
+
+print(name)
