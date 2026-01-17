4acc56c909e1106bb9326c586b7d673490b2410a
Davitisbadatcoding <davitiscool76@gmail.com>
2026-01-17 06:18:15 +0000

    Html > Python

---
+++ Level 28/Homework/homework28.py
@@ -0,0 +1,11 @@
+# funkcia aris iseti codi, romelic shegizlia gamoiyeno ramdenjerac ginda
+# print funkcia aris iseti funkcia, romelic gamoyavs shignit sheyvanil () texts
+# argument aris mag: "Hello World" printis frchxilebshi, anu gadacemili texti da ase shemdeg funkciistvis
+
+name = "Davit"
+age = 15
+job = "Junior Developer"
+skills = "Programming, Quick thinking skills, Problem solver"
+reason = "I need Money!"
+
+print(f"Hello, my name is {name}, and I am {age} years old, I am currently a {job}. My skill set is: {skills}, and why do I need this Job? Because {reason}")
